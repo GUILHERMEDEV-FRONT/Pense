a464846d475beb98925dba9d09f152275bd2a8fa
Guilherme <guilhermerv1909@gmail.com>
2026-02-26 00:27:29 -0300

    fix database config for local and production

--- db/conn.js
+++ db/conn.js
@@ -1,14 +1,24 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+let sequelize
+
+if (process.env.MYSQL_URL) {
+  // Produção (Railway)
+  sequelize = new Sequelize(process.env.MYSQL_URL, {
     dialect: 'mysql'
-  }
-)
+  })
+} else {
+  // Ambiente local
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql'
+    }
+  )
+}
+console.log('CONN FILE CARREGADO')
 
 module.exports = sequelize
