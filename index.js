23b69e5400fd00ba95b801b3096e79776516da6a
Guilherme <guilhermerv1909@gmail.com>
2026-02-25 22:02:58 -0300

    deploy

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+require('dotenv').config()
+const { Sequelize } = require('sequelize')
+const { engine } = require('express-handlebars');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const flash = require('connect-flash');
+
+const app = express();
+
+const conn = require('./db/conn')
+
+
+
+// Models
+const Tought = require('./models/Tought')
+const User = require('./models/User')
+
+//Import Routes
+
+const toughtsRoutes = require('./routes/toughtsRoutes');
+const authRoutes = require('./routes/authRoutes')
+
+// Import Controller
+const ToughtsController = require('./controllers/TougthsController');
+
+// template engine
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+// receber resposta do body
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// session middleware
+app.use(
+    session({
+        name: "session",
+        secret: "nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function () { },
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            
+            expires: new Date(Date.now() + (1000 * 60 * 60 * 5)),
+            httpOnly: true
+        }
+    })
+)
+
+// flash messages
+app.use(flash())
+
+// public path
+app.use(express.static('public'))
+
+// set session to res 
+app.use((req, res, next) => {
+    // Verifique se você está pegando o flash() e passando para o locals
+    res.locals.messages = req.flash();
+    next();
+});
+
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
+
+//Routes
+
+app.use('/toughts', toughtsRoutes)
+app.use('/', authRoutes)
+app.get('/', ToughtsController.showToughts)
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 3000)
+  })
+  .catch((err) => console.log(err))
---
+++ routes/toughtsRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const ToughtsController = require('../controllers/TougthsController')
+
+//helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+
+// controller
+router.get('/add', checkAuth, ToughtsController.createTought)
+router.post('/add', checkAuth, ToughtsController.createToughtSave)
+router.get('/edit/:id', checkAuth, ToughtsController.updateTought)
+router.post('/edit', checkAuth, ToughtsController.updateToughtSave)
+router.get('/dashboard', ToughtsController.dashboard)
+router.post('/remove', checkAuth, ToughtsController.removeTought)
+router.get('/', ToughtsController.showToughts)
+
+module.exports = router
